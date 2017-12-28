115f5898e12daf6fe785ed3c449615aaef99907e
Phoebe Lau <phoebelau7@gmail.com>
2017-12-28 01:50:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,8 +21,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters ON series.id = characters.series_id
-  GROUP BY characters.species
-  COUNT characters.species;"
+  COUNT characters.species WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
