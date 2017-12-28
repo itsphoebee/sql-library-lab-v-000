f9ceb9232372b56d752b8a1a40dbad6df0c1d3b7
Phoebe Lau <phoebelau7@gmail.com>
2017-12-28 02:10:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,5 +30,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id) FROM characters
   INNER JOIN character_books ON characters.id = character_books.character_id
   INNER JOIN books ON books.id = character_books.book_id
-  GROUP BY characters.id;"
+  GROUP BY characters.name;"
 end
